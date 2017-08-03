b46f368be36d81104db61fd5199338ab72a93ba7
cycbote <cycbot@qq.com>
2017-08-03 16:14:28 +0800

    添加多种排序算法

--- js/main.js
+++ js/main.js
@@ -23,8 +23,33 @@ startBtn.addEventListener('click', () => {
       states = selectionSort(originArr)
       animate.start(states)
       break
+    case '冒泡排序':
+      states = bubbleSort(originArr)
+      animate.start(states)
+      break
+    case '插入排序':
+      states = insertionSort(originArr)
+      animate.start(states)
+      break
+    case '快速排序':
+      states = quickSort(originArr)
+      animate.start(states)
+      break
   }
 })
+
+function bubbleSort(arr) {
+  let states = []
+  for (let i = 0, len = arr.length;i < len - 1;i++) {
+    for (let j = 0;j < len - 1 - i;j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j + 1, j)
+        states.push([j + 1, j])
+      }
+    }
+  }
+  return states
+}
 
 function selectionSort(arr) {
   let states = []
@@ -45,6 +70,52 @@ function selectionSort(arr) {
   }
   return states
 }
+
+function insertionSort(arr) {
+  let temp, j, k, states = []
+  for (let i = 1, len = arr.length;i < len;i++) {
+    temp = arr[i]
+    j = i - 1
+    k = i
+    while (j >= 0 && arr[j] > temp) {
+      swap(arr, j, k)
+      states.push([j, k])
+      j--
+      k--
+    }
+  }
+  return states
+}
+
+function quickSort(arr, left = 0, len = arr.length) {
+  let state = []
+  quickSortRecurison(arr, left, len)
+  function quickSortRecurison(ar, left, len) {
+    let pivot
+    if (left < len) {
+      pivot = partition(arr, left, len)
+      quickSortRecurison(arr, left, pivot - 1)
+      quickSortRecurison(arr, pivot, len)
+    }
+    function partition(arr, left, len) {
+      let pivot = left
+      for (let right = left + 1;right < len;right++) {
+        state.push({base: pivot, index: right})
+        if (arr[pivot] >= arr[right]) {
+          left++
+          swap(arr, left, right)
+          state.push([left, right])
+        }
+      }
+      swap(arr, left, pivot)
+      state.push([left, pivot])
+      return left + 1
+    }
+  }
+  return state
+}
+
+
 
 function swap(arr, a, b) {
   let temp
